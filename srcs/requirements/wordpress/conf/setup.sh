313a01736c83e2680f1db350c38a2181dcd82b69
Zouhair Laaroussi <92701395+ZoouhairLrs@users.noreply.github.com>
2024-07-22 19:27:58 +0100

    setup mariadb and open index page default

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -4,17 +4,30 @@ if [ -f ./wp-config.php ]
     then
         echo "Wordpress already downloaded"
     else
-
+    sleep 40
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
+    mkdir -p /var/www/html
+    chmod -R 777 /var/www/html
+    cd /var/www/html
 
     wp core download --allow-root
 
-    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+    >&2 echo $MARIADB_NAME
+    >&2 echo $MARIADB_USER
+    >&2 echo $MARIADB_PWD
+    >&2 echo $MARIADB_HOST
 
-    wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+    wp config create --dbname=$MARIADB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PWD --dbhost=$MARIADB_HOST --allow-root
+
+    wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_USR --admin_password=$WORDPRESS_ADMIN_PWD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=subscriber --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 fi
 
-exec "$@"
+ mkdir -p /var/run/php
+ sed -i 's/listen = /run/php/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+#  /etc/init.d/php7.4-fpm restart
+/usr/sbin/php-fpm7 -F -R
+# exec "$@"
+# tail -f /dev/null
